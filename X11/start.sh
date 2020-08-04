f3cc6a6dedaa9b722a0b2f1a46b2487ac0725f9c
Neeraj <nattavar@auracompany.com>
2020-08-04 20:01:02 +0530

    updates 04Aug20

--- X11/start.sh
+++ X11/start.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+
+# Start dwmblocks
 #exec dwmblocks &
+
+# Start picom compositor
 exec picom &
+
+# Start nitrogen for setting wallpaper
 exec nitrogen --restore &
-exec $XDG_CONFIG_HOME/polybar/launch.sh &
+
+# Start network manager system tray
 exec nm-applet &
 
+# Start battery notification service
+exec $XDG_CONFIG_HOME/dunst/notifications/bat0.sh &
+
+# Start polybar
+exec $XDG_CONFIG_HOME/polybar/launch.sh &
+
+# export variables required for dwm
 export AWT_TOOLKIT=MToolkit
-#exec dwm
+
+# Start dwm
 while true; do
     dwm 2> $XDG_CONFIG_HOME/dwm/dwm.log
 done

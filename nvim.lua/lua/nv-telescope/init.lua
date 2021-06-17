0bdd414556f0b3488166e5a3a263660736a8fcf6
Neeraj <neeraj.a4353@gmail.com>
2021-06-17 14:17:42 +0530

    add telescope custom functions

--- nvim.lua/lua/nv-telescope/init.lua
+++ nvim.lua/lua/nv-telescope/init.lua
@@ -1,3 +1,5 @@
+local utils = require('telescope.utils')
+
 require('telescope').setup{
   defaults = {
     vimgrep_arguments = {
@@ -47,7 +49,53 @@ require('telescope').setup{
   }
 }
 
+
+local M = {}
+
+M.project_files = function()
+    local _, ret, stderr = utils.get_os_command_output({
+        'git', 'rev-parse', '--is-inside-work-tree'
+    })
+    local gopts = {}
+    gopts.prompt_title = ' Git Files'
+    gopts.prompt_prefix = '  '
+    if ret == 0 then
+        require'telescope.builtin'.git_files(gopts)
+    else
+        require'telescope.builtin'.find_files()
+    end
+end
+
+function M.find_files()
+    require('telescope.builtin').find_files {
+        prompt_title = 'Find in DotFiles',
+        shorten_path = false,
+        search_dirs = {
+            '~/.config', '~/Personal/opensource/dotfiles'
+        },
+        cwd = '~/Personal/opensource/dotfiles',
+        width = .25,
+        layout_strategy = 'horizontal',
+        layout_config = {preview_width = 0.65}
+    }
+end
+
+function M.file_explorer()
+    require('telescope.builtin').file_browser {
+        prompt_title = ' File Browser',
+        shorten_path = false,
+        cwd = '~',
+        width = .25,
+        layout_strategy = 'horizontal',
+        layout_config = {preview_width = 0.65}
+    }
+end
+
 vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Leader>fd', ':lua require(\'nv-telescope\').find_files()<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Leader>fb', ':lua require(\'nv-telescope\').file_explorer()<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Leader>fp', ':lua require(\'nv-telescope\').project_files()<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<Leader>fg', ':Telescope live_grep<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope buffers<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<Leader>fh', ':Telescope help_tags<CR>', {noremap = true, silent = true})
+
+return M

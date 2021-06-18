ae140999b6248151ec673b31164202dc416f93d5
Neeraj <neeraj.a4353@gmail.com>
2021-06-18 15:55:03 +0530

    cleanups on saga and lspinstall

--- nvim.lua/lua/nv-lspsaga/init.lua
+++ nvim.lua/lua/nv-lspsaga/init.lua
@@ -2,27 +2,20 @@ local saga = require 'lspsaga'
 saga.init_lsp_saga()
 
 vim.api.nvim_set_keymap("n", "<Leader>clf", ":Lspsaga lsp_finder<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>cca", ":Lspsaga code_action<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("v", "<Leader>cca", ":<C-U>Lspsaga range_code_action<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>chd", ":Lspsaga hover_doc<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>cld", ":Lspsaga show_line_diagnostics<CR>", {noremap = true, silent = true})
---[[ vim.api.nvim_set_keymap("n", "<Leader>clf", ":Lspsaga lsp_finder<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>clf", ":Lspsaga lsp_finder<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>clf", ":Lspsaga lsp_finder<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>clf", ":Lspsaga lsp_finder<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>clf", ":Lspsaga lsp_finder<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>clf", ":Lspsaga lsp_finder<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>clf", ":Lspsaga lsp_finder<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>clf", ":Lspsaga lsp_finder<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>clf", ":Lspsaga lsp_finder<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>clf", ":Lspsaga lsp_finder<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>ca", ":Lspsaga code_action<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("v", "<Leader>ca", ":<C-U>Lspsaga range_code_action<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>k", ":Lspsaga hover_doc<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>sd", ":Lspsaga show_line_diagnostics<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<C-k>", ":Lspsaga signature_help<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>lr", ":Lspsaga rename<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<C-f>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<C-b>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>pd", ":Lspsaga preview_definition<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "[d", ":Lspsaga diagnostic_jump_next<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "]d", ":Lspsaga diagnostic_jump_prev<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>ot", ":Lspsaga open_floaterm<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("t", "<Leader>ct", "<C-\\><C-n>:Lspsaga close_floaterm<CR>", {noremap = true, silent = true})
 
-nnoremap <silent><C-f> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>
-nnoremap <silent><C-b> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>
-nnoremap <silent><leader>csh :Lspsaga signature_help<CR>
-nnoremap <silent><leader>crn :Lspsaga rename<CR>
-nnoremap <silent><leader>cpd:Lspsaga preview_definition<CR>
-nnoremap <silent> [e :Lspsaga diagnostic_jump_next<CR>
-nnoremap <silent> ]e :Lspsaga diagnostic_jump_prev<CR>
+--[[
 nnoremap <silent> <leader>cot :Lspsaga open_floaterm<CR>
 tnoremap <silent> <leader>cct <C-\><C-n>:Lspsaga close_floaterm<CR> ]]
--- nvim.lua/lua/nv-telescope/init.lua
+++ nvim.lua/lua/nv-telescope/init.lua
@@ -39,6 +39,7 @@ require('telescope').setup{
     border = {},
     color_devicons = true,
     use_less = true,
+    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
     set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
     file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
     grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,

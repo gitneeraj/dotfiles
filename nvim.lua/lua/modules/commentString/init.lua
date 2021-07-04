806bc37b75d575ad6af7227a34a389712b75bf48
Neeraj <neeraj.a4353@gmail.com>
2021-07-04 18:11:16 +0530

    fix commentstring for svelte

--- nvim.lua/lua/modules/commentString/init.lua
+++ nvim.lua/lua/modules/commentString/init.lua
@@ -5,14 +5,10 @@ require('kommentary.config').configure_language('javascriptreact', {
 })
 
 require('kommentary.config').configure_language('svelte', {
+    single_line_comment_string = 'auto',
+    multi_line_comment_strings = 'auto',
     hook_function = function()
         require('ts_context_commentstring.internal').update_commentstring()
     end
 })
-
---[[ require('kommentary.config').configure_language('default', {
-    hook_function = function()
-        require('ts_context_commentstring.internal').update_commentstring()
-    end
-}) ]]
 

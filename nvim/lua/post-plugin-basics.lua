0ee43e6e5a2dcecadbe820fc0783146b54f41e84
gitneeraj <36226187+gitneeraj@users.noreply.github.com>
2021-12-16 12:01:21 +0530

    remove nvim config (#12)
    Co-authored-by: Neeraj A <neeraj.a@LT360.local>

--- nvim/lua/post-plugin-basics.lua
+++
@@ -1,9 +0,0 @@
--- ################# Basic settings dependent on plugins ################ --
-
--- ================= Visualization ================= --
-
-vim.o.termguicolors = true
-vim.o.background = 'dark'
-vim.cmd('colorscheme onedark')
-vim.api.nvim_command('let g:palenight_terminal_italics=1')
-
